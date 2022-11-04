1b6cd8de457163ab95378d4b30c6fe01a62563de
JelloMangune <mangune.jello@auf.edu.ph>
2022-11-04 16:39:14 +0800

    first commit

--- script.js
+++ script.js
@@ -17,25 +17,3 @@ let car = {
     }
 }
 
-class Car{
-    constructor(brand, variant, transmission){
-        this.brand = brand;
-        this.variant = variant;
-        this.transmission = transmission;
-    }
-}
-
-function vehicle(){
-    this.vehicleType="vehicleType";
-}
-
-vehicle.prototype.display=function(){
-    return "The brand is: " + this.vehicleType;
-}
-
-function MyCar(vehicleType){
-    this.vehicleType = vehicleType;
-}
-
-MyCar.prototype=Object.create(vehicle.prototype);
-var myCar = new MyCar("Toyota");
